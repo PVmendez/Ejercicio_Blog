455c137af92fa893f6b96ba2283f3b673033d064
Sebastián Guadalupe <sebastianguadalupe00@gmail.com>
2022-07-29 16:38:22 -0300

    blog Controller

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -4,31 +4,50 @@ const blogController = {
 	index: async (req, res) => {
 		console.log(Article);
 		const blogs = await Article.findAll();
-		res.render("blog", { blogs });
+		res.render("home", { blogs });
 	},
 	create: async function (req, res) {
-		const blogs = await Article.findAll();
-		res.render("");
+		res.render("create");
 	},
 	store: async function (req, res) {
-		const blogs = await Article.findAll();
-		res.render("");
+		console.log(req.body);
+		const { title, content, image } = req.body;
+		const date = new Date();
+		const blogs = await Article.create({ title, content, image, date });
+		res.redirect("/admin");
 	},
 	show: async function (req, res) {
 		const blogs = await Article.findAll();
 		res.render("");
 	},
 	edit: async function (req, res) {
-		const blogs = await Article.findAll();
-		res.render("");
+		const blog = await Article.findByPk(req.params.id);
+		res.render("edit", { blog });
 	},
 	update: async function (req, res) {
-		const blogs = await Article.findAll();
-		res.render("");
+		const { title, content, image } = req.body;
+		const date = new Date();
+		const blog = await Article.update(
+			{ title, content, image, date },
+			{
+				where: {
+					id: req.params.id,
+				},
+			}
+		);
+		res.redirect("/admin");
 	},
 	destroy: async function (req, res) {
+		const blogs = await Article.destroy({
+			where: {
+				id: req.params.id,
+			},
+		});
+		res.redirect("/admin");
+	},
+	admin: async function (req, res) {
 		const blogs = await Article.findAll();
-		res.render("");
+		res.render("admin", { blogs });
 	},
 };
 
