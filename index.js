c22d74c3a530fd67ef28a8f1c9cadb48014428ae
PVmendez <valentino.mendez.rey@gmail.com>
2022-07-29 14:35:00 -0300

    Blog screen

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 require("dotenv").config();
 
-const app = process.env.APP_PORT;
-console.log(app);
+const express = require("express");
+const APP_PORT = process.env.APP_PORT || 3000;
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.get('/articulo/:id', (req, res) => {
+  res.render('blog');
+})
+
+app.listen(APP_PORT, () => {
+  console.log(`\n[Express] Servidor corriendo en el puerto ${APP_PORT}.`);
+  console.log(`[Express] Ingresar a http://localhost:${APP_PORT}/articulo.\n`);
+});
