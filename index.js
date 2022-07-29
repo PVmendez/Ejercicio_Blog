1857e47a2e44306690b2efb731645ed4195f3498
Mechu Torrendell <mechutorrendell@Mechus-MacBook-Air.local>
2022-07-29 14:51:36 -0300

    Home

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 require("dotenv").config();
-
-const app = process.env.APP_PORT;
+const express = require("express");
+const app = express();
+const port = process.env.APP_PORT;
 console.log(app);
+
+app.set("view engine", "ejs");
+
+app.get("/usuarios", (req, res) => {
+    res.render("home", { nothing: "hola" })
+})
+//se va 
+
+app.listen(3000, (req, res) => {
+    console.log(`listening in port http://localhost:3000/usuarios`)
+})
